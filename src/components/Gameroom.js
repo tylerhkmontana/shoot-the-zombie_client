15fd29ea45a69b10db41e0e7cb1970be1aee8460
Hyunbin Kim <Hyunbin>
2020-11-12 16:23:54 -0500

    style update

--- src/components/Gameroom.js
+++ src/components/Gameroom.js
@@ -4,7 +4,7 @@ import { useHistory } from 'react-router'
 import { updateUserList } from '../actions/gameroom.action'
 import { socket } from '../App'
 import Icon from '@mdi/react'
-import { mdiMinusBox, mdiPlusBox } from '@mdi/js'
+import { mdiMinusBox, mdiPlusBox, mdiBullet } from '@mdi/js'
 import "./Gameroom.css"
  
 function Gameroom() {
@@ -16,7 +16,7 @@ function Gameroom() {
   const [isRoomMaster, setIsRoomMaster] = useState(false)
   const [gameSetting, setGameSetting] = useState({
     numBullets: 1,
-    infectionRate: 5000
+    infectionRate: 10000
   })
   
   useEffect(() => {
@@ -55,72 +55,132 @@ function Gameroom() {
     push("/menu")
   }
 
+  function setNumBullets(isIncrease) {
+    let numBullets = gameSetting.numBullets
+    let didReachMax = numBullets === 3 && isIncrease
+    let didReachMin = numBullets === 0 && !isIncrease
+
+    if (!didReachMax && !didReachMin) {
+      setGameSetting({
+        ...gameSetting,
+        numBullets: isIncrease ? ++numBullets : --numBullets
+      })
+    }
+  }
+
+  function setInfectionRate(isIncrease) {
+    let infectionRate = gameSetting.infectionRate
+    let didReachMax = infectionRate === 30000 && isIncrease
+    let didReachMin = infectionRate === 10000 && !isIncrease
+    
+    if (!didReachMax && !didReachMin) {
+      setGameSetting({
+        ...gameSetting,
+        infectionRate: isIncrease ? infectionRate + 1000 : infectionRate - 1000
+      })
+    }
+  }
+
   return (
     <div className="Gameroom">
+
       <div className="room-info">
         <h4>TITLE: {roomInfo.roomTitle}</h4>
         <h4>ROOM_CODE: {roomInfo.roomcode}</h4>
       </div>
     
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column'
-      }}>
-        <div className="user-list">
-          <p>{`${roomInfo.players.length}/${roomInfo.numPlayers}`}</p>
-          {
-            roomInfo.players.map((player, i) => {
-              if (i === 0 && player.id === userId) {
-                return <p key={player.id}>{player.userName}(YOU)(Master)</p>
-              } else if (i === 0) {
-                return <p key={player.id}>{player.userName}(Master)</p>
-              } else if (player.id === userId) {
-                return <p key={player.id}>{player.userName}(YOU)</p>
-              } else {
-                return <p key={player.id}>{player.userName}</p>
-              } 
-            })
-          }
+      <div className="user-list">
+        <span
+          style={{
+            position: 'sticky', 
+            top: 0, 
+            textAlign: 'center',
+            backgroundColor: 'black',
+            width: '100%',
+            zIndex: 1
+        }}>
+          {`${roomInfo.players.length}/${roomInfo.numPlayers}`}
+        </span>
+        {
+          roomInfo.players.map((player, i) => {
+            if (i === 0 && player.id === userId) {
+              return <p key={player.id}>{player.userName}(YOU)(Master)</p>
+            } else if (i === 0) {
+              return <p key={player.id}>{player.userName}(Master)</p>
+            } else if (player.id === userId) {
+              return <p key={player.id}>{player.userName}(YOU)</p>
+            } else {
+              return <p key={player.id}>{player.userName}</p>
+            } 
+          })
+        }
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+        <p>asdfasdfasdfasdf</p>
+      </div>
+
+      <div className="game-setting">
+        <div className="setting">
+          <p style={{flex: 1}}>Infection_rate:</p>
+          <div style={{display: 'flex', justifyContent: 'space-between', flex: 1}}>
+            <Icon 
+              path={mdiMinusBox} 
+              size={1}
+              color="#00fe00"
+              onClick={() => setInfectionRate(false)}/>
+            <p>{gameSetting.infectionRate /1000} s</p>
+            <Icon 
+              path={mdiPlusBox}
+              size={1}
+              color="#00fe00"
+              onClick={() => setInfectionRate(true)}/>
+          </div>
         </div>
 
-        <div className="game-setting">
-          <div className="infection-rate-setting">
-            <p style={{flex: 1}}>Infection_rate:</p>
-            <div style={{display: 'flex', justifyContent: 'space-between', flex: 1}}>
-              <Icon 
-                path={mdiMinusBox} 
+        <div className="setting">
+          <p style={{flex: 1}}>Initial_#bullets:</p>
+          <div style={{display: 'flex', justifyContent: 'space-between', flex: 1}}>
+            <Icon 
+              path={mdiMinusBox} 
+              size={1}
+              color="#00fe00"
+              onClick={() => setNumBullets(false)}/>
+            <span
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+              <Icon
+                path={mdiBullet}
                 size={1}
                 color="#00fe00"/>
-              <p>{gameSetting.infectionRate /1000} s</p>
-              <Icon 
-                path={mdiPlusBox}
-                size={1}
-                color="#00fe00"/>
-            </div>
+                X {gameSetting.numBullets}
+            </span>
+            <Icon 
+              path={mdiPlusBox}
+              size={1}
+              color="#00fe00"
+              onClick={() => setNumBullets(true)}/>
           </div>
-          
-          {/* {
-            isRoomMaster ? 
-            <div>
-              <input placeholder="# of bullets" type="number"/>
-              <select onChange={event => console.log(event.target.value)}>
-                <option value="5000">5 sec</option>
-                <option value="10000">10 sec</option>
-                <option value="15000">15 sec</option>
-              </select>
-            </div> :
-            ''
-          } */}
-          <div>
-            <button onClick={exitRoom}>Exit</button>
-            { 
-              isRoomMaster ? 
-                <button onClick={startGame}>Start</button> :
-                ''
-            }
-          </div> 
         </div>
+        
       </div>
+      <div className="button-group">
+          {
+            isRoomMaster ?
+              <button onClick={startGame}>Start</button> :
+              ''
+          }
+          <button onClick={exitRoom}>Exit</button>
+        </div>
     </div>
   )
 }
